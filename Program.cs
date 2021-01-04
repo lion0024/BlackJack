a3136b0dea823d8768a9fedbb0de5c1841e6cfb4
正晃 <lion0024.xyz@gmail.com>
2021-01-04 19:47:12 +0900

    カードクラスの作成

--- Program.cs
+++ Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BlackJack
 {
+    public class Card
+    {
+        public string Mark { get; set; }
+
+        // 1,トランプの数値
+        public int No { get; set; }
+
+        // 2,トランプの表示
+        public string NoString
+        {
+            get
+            {
+                // 1,トランプの数値を使用して判定する
+                switch (No)
+                {
+                    case 1:
+                        return "A";
+                    case 11:
+                        return "J";
+                    case 12:
+                        return "Q";
+                    case 13:
+                        return "K";
+                }
+
+                return No.ToString();
+            }
+        }
+
+        // 3,ブラックジャックの点
+        public int Point
+        {
+            get
+            {
+                // 1,トランプの数値を利用して判定
+                switch (No)
+                {
+                    case 11:
+                        return 10;
+                    case 12:
+                        return 10;
+                    case 13:
+                        return 10;
+                }
+
+                return No;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Hello World");
         }
     }
 }
