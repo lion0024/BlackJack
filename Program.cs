06ef10afa7b99d78925f10b7aba9d04bf7f2661e
正晃 <lion0024.xyz@gmail.com>
2021-01-04 19:07:49 +0900

    First Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
